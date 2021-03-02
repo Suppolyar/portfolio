19d09415daf1a40b6607dc5cb69768b63f0950fe
Ilya <vkhardy@inbox.ru>
2021-03-02 03:47:04 +0300

    v 1.1 added menu

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+const hamburger    = document.querySelector('.hamburger'),
+      menu         = document.querySelector('.menu'),
+      closeElem    = document.querySelector('.menu__close'),
+      closeOverlay = document.querySelector('.menu__overlay');
+      
+hamburger.addEventListener('click',() => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click',() => {
+    menu.classList.remove('active');
+});
+
+closeOverlay.addEventListener('click',() => {
+    menu.classList.remove('active');
+});

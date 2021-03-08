fa844d8107be513656182e74ce1a03e0c81813ef
Ilya <vkhardy@inbox.ru>
2021-03-08 20:50:07 +0300

    v.1.5 added php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const { data } = require("jquery");
+
 const hamburger    = document.querySelector('.hamburger'),
       menu         = document.querySelector('.menu'),
       closeElem    = document.querySelector('.menu__close'),
@@ -13,4 +15,38 @@ closeElem.addEventListener('click',() => {
 
 closeOverlay.addEventListener('click',() => {
     menu.classList.remove('active');
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+    })
+    return false;
+});
+
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      // отменяем стандартное действие
+      event.preventDefault();
+      
+      var sc = $(this).attr("href"),
+          dn = $(sc).offset().top;
+      /*
+      * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+      * dn - определяем положение блока на странице
+      */
+      
+      $('html, body').animate({scrollTop: dn}, 1000);
+      
+      /*
+      * 1000 скорость перехода в миллисекундах
+      */
+    });
 });
